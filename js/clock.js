d197cbf1cb550c37fe058d4a46a20066a7389430
denefc <mehmet_7kezman@hotmail.com>
2021-05-11 23:14:40 +0300

    first homework done

---
+++ js/clock.js
@@ -0,0 +1,20 @@
+const clock=document.querySelector('#myClock')
+
+function showTime(){
+    let date=new Date();
+    let hour=date.getHours();
+    let minutes=date.getMinutes();
+    let seconds=date.getSeconds();
+    if(minutes<10){
+        minutes="0"+minutes
+    }
+
+    let time=`${hour}: ${minutes}: ${seconds} `
+
+    
+    clock.innerHTML=time
+    setTimeout(showTime,1000)
+    
+}
+
+clock.addEventListener("onload",showTime())
